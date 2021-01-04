74860ddb25ea2c92f649a47fcea215eef77f4498
test@gmail.com <test@gmail.com>
2021-01-04 20:08:16 +0300

    help message in admin panel for image

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,11 +1,31 @@
 
 from django.contrib import admin
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField, ModelForm, ValidationError
+
+from PIL import Image 
 
 from .models import *
 
 
+class NotebookAdminForm(ModelFrom):
+
+	MIN_RESOLUTION = (400, 400)
+
+	def __init__(self, *args, **kwargs):
+	    super().__init__(*args, **kwargs)
+	    self.fields['image'].help_text = 'Загружайте изображенмя с минимальным разрешением {}x{}'.format(*self.MIN_RESOLUTION)
+
+	def clean_image(self):
+		image = sefl.cleaned_data['image']
+		img = Image.open(image)
+		min_height, min_width = self.MIN_RESOLUTION
+		if img.height < min_height or img.width < min_width:
+			raise ValidationError('Разрещение изображенмя меньше минимального')
+		return image
+
 class NotebookAdmin(admin.ModelAdmin):
+
+	form = NotebookAdmin
 
 	def formfield_for_foreignkey(self, db_field, request, **kwargs):
 		if db_field.name == 'category':
